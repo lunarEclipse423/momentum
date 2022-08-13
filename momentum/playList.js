99ab600d846899288a43b229db8a95c856112509
Maria White <redstone.mari@gmail.com>
2022-08-13 15:19:43 +0300

    fix: change music for playlist

--- momentum/playList.js
+++ momentum/playList.js
@@ -1,29 +1,39 @@
 const playList = [
-    {      
-        title: 'Aqua Caelestis',
-        src: './assets/sounds/Aqua Caelestis.mp3',
-        duration: '00:58'
-    },  
-    {      
-        title: 'River Flows In You',
-        src: './assets/sounds/River Flows In You.mp3',
-        duration: '03:50'
-    }, 
-    {      
-        title: 'Ennio Morricone',
-        src: './assets/sounds/Ennio Morricone.mp3',
-        duration: '01:37'
-    }, 
-    {      
-        title: 'Summer Wind',
-        src: './assets/sounds/Summer Wind.mp3',
-        duration: '01:50'
+    {
+        title: 'Pequod',
+        src: './assets/sounds/Pequod.mp3',
+        duration: '01:30'
+    },
+    {
+        title: 'Hero\'s Come Back!!',
+        src: './assets/sounds/Hero\'s Come Back!!.mp3',
+        duration: '04:33'
     }, 
     {
-        title: 'shot',
+        title: 'The Rock City Boy',
+        src: './assets/sounds/The Rock City Boy.mp3',
+        duration: '01:27'
+    },
+    {
+        title: 'Goya no Machiawase',
+        src: './assets/sounds/Goya no Machiawase.mp3',
+        duration: '02:50'
+    },
+    {
+        title: 'Torch Of Liberty',
+        src: './assets/sounds/Torch Of Liberty.mp3',
+        duration: '03:05'
+    },
+    {
+        title: 'Reiner, sit down',
+        src: './assets/sounds/Reiner, sit down.mp3',
+        duration: '00:22'
+    }, 
+    {
+        title: 'Shot',
         src: './assets/sounds/shot.mp3',
         duration: '00:02'
-    }
+    },
 ];
 
 export default playList;
